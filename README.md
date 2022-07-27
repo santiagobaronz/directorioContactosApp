a409f1b991ebd905aa78bb919bccfbe00a7ab92f
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2022-07-26 22:41:49 -0500

    [First commit]

---
+++ README.md
@@ -0,0 +1,27 @@
+# 💰 Contact directory App
+
+The application allows to save, filter and delete contacts that a user adds.
+
+## Installation 💻💡
+
+To install the project on your computer, you can use the "Code" button and follow the following steps in each case:
+
+### 1️⃣ Clone the project
+
+- Copy the URL
+- Open your Git Bash
+- Write ``` git clone ``` and paste the url. As follows:
+
+``` 
+git clone https://github.com/santiagobaronz/directorioContactosApp.git
+```
+
+### 2️⃣ Download ZIP
+
+- Click "Download ZIP"
+- Download the ZIP and extract the files into a folder
+- Open the folder in your favorite IDE
+
+## Authors 🦸‍♀️
+
+- Santiago Baron Zuleta - *Project creator* [Santiago Baron Zuleta](https://github.com/santiagobaronz/)
---
+++ src/main/java/com/mycompany/directorio/Contacto.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.directorio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Katherine Ospina
+ */
+public class Contacto {
+    
+    String nombre;// Atributo de la clase 
+    String telefono;
+    String direccionpostal;
+    String correoelectronico; 
+    
+    ArrayList<Contacto> listaContactos= new ArrayList<>();//Llamamos al constructor
+    
+    public Contacto( String nombrec, String telefono,String direccionpostal,String correoelectronico){
+        
+         this.nombre = nombrec; //Asignar al atributo de clase la variable local
+         this.telefono = telefono;
+         this.direccionpostal = direccionpostal;
+         this.correoelectronico = correoelectronico;
+        
+    }
+    
+    public Contacto (){
+        
+        
+    }
+    
+    public void agregarContacto ( String nombrec,String telefono,String direccionpostal,String correoelectronico){
+        Contacto nuevoContacto;
+        nuevoContacto=new Contacto(nombrec,telefono,direccionpostal,correoelectronico);
+        listaContactos.add(nuevoContacto);
+    }
+}
